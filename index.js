bdd50428b1273e27fc879dcb8a754b510827e00b
Otter <otc@otterthecat.com>
2016-01-01 22:46:22 -0600

    refactor for easier readability. update tests accordingly

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 var particleBeam = require('./lib/particle-beam');
 
+/*
+  'config' is an object with following properties:
+    - device: ID of specific board you wish to control
+    - token: ID issued by Particle to allow access to your devices
+*/
 module.exports = function (config) {
   return particleBeam.bind(config);
 };
--- test/specs/particle-beamSpec.js
+++ test/specs/particle-beamSpec.js
@@ -12,18 +12,19 @@ chai.use(sinonChai);
 // /////////////////////////////////////////////////////////
 var beam = require('../../lib/particle-beam');
 var stubConfig = {
-  "device": '1234',
-  "token": '5678'
+  'device': '1234',
+  'token': '5678'
 };
 
 describe('particle-beam', function () {
-  it('should be a function', function () {
-    beam.should.be.a('function');
+  it('should return a function', function () {
+    beam(stubConfig).should.be.a('function');
   });
 
   describe('when called', function () {
-    it('should throw an error if no arguments are passed', function(){
-      beam.bind(stubConfig).should.throw(Error);
+    it('should throw an error if no arguments are passed', function () {
+      var b = beam(stubConfig);
+      beam().should.throw(Error);
     });
   });
 });
